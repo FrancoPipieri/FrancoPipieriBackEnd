497e6816d3b607bbfd91a6b5499f2d2c7a92d935
Franco Pipieri <pipierif98@gmail.com>
2022-12-16 00:33:33 -0300

    HBS

---
+++ PUG-EJS/HANDLEBARS/src/server.js
@@ -0,0 +1,32 @@
+import express, { json, urlencoded } from 'express';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { engine } from "express-handlebars";
+import router from "./routes/index.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(json());
+
+app.use(urlencoded({ extended: true }));
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main.hbs',
+    layoutsDir: join(__dirname, '/views/layouts'),
+    partialsDir: join(__dirname, '/views/partials')
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', join(__dirname, '/views'));
+
+app.use('/', router)
+
+app.listen('8080', () => {
+    console.log("server listening port 8080");
+})
+
+app.on('error', (err) => {
+    console.log(err);
+})
