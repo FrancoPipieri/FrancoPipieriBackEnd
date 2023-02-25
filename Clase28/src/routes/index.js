535378a8fd51f3b1b3f0fb2fe31029106ce38f51
Franco Pipieri <pipierif98@gmail.com>
2023-02-24 22:52:33 -0300

    Clase 28

---
+++ Clase28/src/routes/index.js
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import passport from "passport";
+import { authController } from "../controllers/index.js";
+import generateFaker from '../faker.js';
+
+const router = Router()
+
+router
+    .route('/login')
+    .get(authController.getLogin)
+    .post(
+        passport.authenticate("login", { failureRedirect: "/fail-login" }),
+        authController.getLogin
+    );
+
+router
+    .route("/register")
+    .get(authController.getRegister)
+    .post(
+        passport.authenticate("register", { failureRedirect: "/fail-register" }),
+        authController.getLogin
+    );
+
+router.get("/fail-login", authController.getLoginFailiure);
+router.get("/fail-register", authController.getRegisterFailiure);
+
+router.get("/logout", authController.logOut);
+
+router.get('/login/productos', (req, res) => {
+    const  user = req.session.passport.user.username;
+    if (!user){ 
+        return res.redirect('/login')
+    }else{
+        return res.render('form', { user })
+    }
+})
+
+router.route('/api/productos-test').get((req, res) => {
+    res.render('test', { items: generateFaker() })
+})
+
+router.get("/info", authController.info)
+
+router.get("/api/randoms", authController.getRandom)
+
+export default router;
